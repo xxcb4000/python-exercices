fb4cf2f5d10932f618c1a6cd4f4bea49318a7b00
xxcb4000 <vincent.bonhomme@gmail.com>
2025-03-25 13:35:26 +0100

    v0.0.4 la tête bouge... constamment dans la direction indiquée

--- pysnake.py
+++ pysnake.py
@@ -4,83 +4,89 @@ import pygame
 pygame.init()
 
 #gestion de la fenêtre (objet)
-class Fenetre:
-    def __init__(self, hauteur, largeur, couleur_de_fond, horloge, pas):
+class fenetre:
+    def __init__(self, hauteur, largeur, couleur_de_fond, fps, pas):
         self.hauteur = hauteur
         self.largeur = largeur
         self.couleur_de_fond = couleur_de_fond
-        self.horloge = horloge
+        self.fps = fps
+        self.horloge = pygame.time.Clock()
         self.pas = pas
+    
+    def tick(self):
+        self.horloge.tick(self.fps)
 
-ma_fenetre_de_jeu = Fenetre(600,600,(255,255,255), 60, 8)
+ma_fenetre_de_jeu = fenetre(600,600,(255,255,255), 10, 5)
 screen = pygame.display.set_mode((ma_fenetre_de_jeu.largeur,ma_fenetre_de_jeu.hauteur))
 
 #gestion d'un anneau (objet)
 class unAnneauDuSerpent:
-    def __init__ (self, position_x = ma_fenetre_de_jeu.largeur/2, position_y = ma_fenetre_de_jeu.hauteur/2, taille = 8, couleur = (0,255,0)):
+    def __init__ (self, rayon = 5, couleur = (0,255,0)):
+        self.rayon = rayon
+        self.couleur = couleur
+
+class serpent:
+    def __init__(self, taille = 1, direction = 'droite', position_x = ma_fenetre_de_jeu.largeur/2, position_y = ma_fenetre_de_jeu.hauteur/2):
         self.position_x = position_x
         self.position_y = position_y
         self.taille = taille
-        self.couleur = couleur
+        self.direction = direction
+        self.anneau = unAnneauDuSerpent()
+        #self.liste_anneaux = []
 
-    def se_deplacer_vers_la_gauche(self):
-        self.position_x -= ma_fenetre_de_jeu.pas
-    def se_deplacer_vers_la_droite(self):
-        self.position_x += ma_fenetre_de_jeu.pas
-    def se_deplacer_vers_le_haut(self):
-        self.position_y -= ma_fenetre_de_jeu.pas
-    def se_deplacer_vers_le_bas(self):
-        self.position_y += ma_fenetre_de_jeu.pas
+    
+    def deplacer(self):
+        if self.direction == 'haut':
+            self.position_y -= ma_fenetre_de_jeu.pas
+
+        elif self.direction == "bas":
+            self.position_y += ma_fenetre_de_jeu.pas
+
+        elif self.direction == "droite":
+            self.position_x += ma_fenetre_de_jeu.pas
+
+        elif self.direction == "gauche":
+            self.position_x -= ma_fenetre_de_jeu.pas
+
+    # def grandir(self):
+    #     self.taille += 1
+    #     self.liste_anneaux.append(self.taille)
+        
     def afficher(self):
-        pygame.draw.circle(screen, self.couleur, (self.position_x,self.position_y), self.taille)
+        #for anneau in self.liste_anneaux:
+        pygame.draw.circle(screen, self.anneau.couleur, (self.position_x, self.position_y), self.anneau.rayon)
 
-ma_tete_de_serpent = unAnneauDuSerpent()
-DEPLACEMENT_SERPENT = pygame.USEREVENT + 1
 
-#gestion du jeu
-class jeu:
-    def __init__(self, running, direction, vitesse):
-        self.running = True
-        self.direction = 'Droite'
-        self.vitesse = 200
-        pygame.time.set_timer(DEPLACEMENT_SERPENT, self.vitesse)
-
-    def gestion_evenements(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                self.running = False
-
-            elif event.type == DEPLACEMENT_SERPENT:
-                ma_tete_de_serpent.se_deplacer_vers_la_droite()
-
-            elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_UP:
-                    ma_tete_de_serpent.se_deplacer_vers_le_haut()
-                elif event.key == pygame.K_DOWN:
-                    ma_tete_de_serpent.se_deplacer_vers_le_bas()
-                elif event.key == pygame.K_LEFT:
-                    ma_tete_de_serpent.se_deplacer_vers_la_gauche()
-                elif event.key == pygame.K_RIGHT:
-                    ma_tete_de_serpent.se_deplacer_vers_la_droite()
-
-    def afficher(self):
-        screen.fill((255,255,255))
-        ma_tete_de_serpent.afficher()
-        pygame.display.flip()
-        pygame.time.Clock().tick(60)
-
-mon_jeu = jeu(True, 'Droite', 200)
+mon_serpent = serpent()
 
 
 
+running = True
 #boucle principale du jeu
-while mon_jeu.running:
+while running:
     #gestion des événements
-    mon_jeu.gestion_evenements()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
+        # elif event.type == DEPLACEMENT_SERPENT:
+        #     ma_tete_de_serpent.se_deplacer_vers_la_droite()
 
-    #affichage à l'écran
-    mon_jeu.afficher()
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_UP:
+                mon_serpent.direction = 'haut'
+            elif event.key == pygame.K_DOWN:
+                mon_serpent.direction = 'bas'
+            elif event.key == pygame.K_LEFT:
+                mon_serpent.direction = 'gauche'
+            elif event.key == pygame.K_RIGHT:
+                mon_serpent.direction = 'droite'
+    
+    screen.fill(ma_fenetre_de_jeu.couleur_de_fond)
+    mon_serpent.deplacer()
+    mon_serpent.afficher()
+    pygame.display.flip()
+    ma_fenetre_de_jeu.tick()
 
 
 pygame.quit()
