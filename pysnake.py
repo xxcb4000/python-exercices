3a933c68be4c2b52ee0656474f2bfd171c5767aa
xxcb4000 <vincent.bonhomme@gmail.com>
2025-03-25 15:19:14 +0100

    v0.0.6 on garde l'historique des positions

--- pysnake.py
+++ pysnake.py
@@ -16,26 +16,24 @@ class fenetre:
     def tick(self):
         self.horloge.tick(self.fps)
 
-ma_fenetre_de_jeu = fenetre(600,600,(255,255,255), 10, 5)
+ma_fenetre_de_jeu = fenetre(600,600,(255,255,255), 10, 10)
 screen = pygame.display.set_mode((ma_fenetre_de_jeu.largeur,ma_fenetre_de_jeu.hauteur))
 
 #gestion d'un anneau (objet)
 class unAnneauDuSerpent:
-    def __init__ (self, rayon = 5, couleur = (0,255,0)):
-        self.rayon = rayon
-        self.couleur = couleur
-
-class serpent:
-    def __init__(self, taille = 1, direction = 'droite', position_x = ma_fenetre_de_jeu.largeur/2, position_y = ma_fenetre_de_jeu.hauteur/2):
+    def __init__ (self, rayon = 5, couleur = (0,255,0), position_x = 300, position_y = 300, direction = 'droite', taille = 1):
         self.position_x = position_x
         self.position_y = position_y
-        self.taille = taille
+        self.rayon = rayon
+        self.couleur = couleur
         self.direction = direction
-        self.anneau = unAnneauDuSerpent()
-        #self.liste_anneaux = []
+        self.taille = taille
+        self.former_position_x = []
+        self.former_position_y = []
 
-    
-    def deplacer(self):
+    def creer(self):
+        self.former_position_x.append(self.position_x)
+        self.former_position_y.append(self.position_y)
         if self.direction == 'haut':
             if self.position_y > 0 :
                 self.position_y -= ma_fenetre_de_jeu.pas
@@ -60,43 +58,43 @@ class serpent:
             else:
                 self.position_x = ma_fenetre_de_jeu.largeur
 
-    # def grandir(self):
-    #     self.taille += 1
-    #     self.liste_anneaux.append(self.taille)
+    def dessiner(self):            
+        pygame.draw.circle(screen, self.couleur, (self.position_x, self.position_y), self.rayon)
+
+    def grandir(self):
+        for i in range(0, self.taille):
+            pygame.draw.circle(screen, self.couleur, (self.former_position_x[i], self.former_position_y[i]), self.rayon)
         
-    def afficher(self):
-        #for anneau in self.liste_anneaux:
-        pygame.draw.circle(screen, self.anneau.couleur, (self.position_x, self.position_y), self.anneau.rayon)
-
-
-mon_serpent = serpent()
-
-
-
+        self.taille += 1  
+    
+        
 running = True
+nouvel_anneau = unAnneauDuSerpent()
 #boucle principale du jeu
 while running:
     #gestion des événements
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-        # elif event.type == DEPLACEMENT_SERPENT:
-        #     ma_tete_de_serpent.se_deplacer_vers_la_droite()
-
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_UP and mon_serpent.direction != 'bas':
-                mon_serpent.direction = 'haut'
-            elif event.key == pygame.K_DOWN and mon_serpent.direction != 'haut':
-                mon_serpent.direction = 'bas'
-            elif event.key == pygame.K_LEFT and mon_serpent.direction != 'droite':
-                mon_serpent.direction = 'gauche'
-            elif event.key == pygame.K_RIGHT and mon_serpent.direction != 'gauche':
-                mon_serpent.direction = 'droite'
+            if event.key == pygame.K_UP and nouvel_anneau.direction != 'bas':
+                nouvel_anneau.direction = 'haut'
+            elif event.key == pygame.K_DOWN and nouvel_anneau.direction != 'haut':
+                nouvel_anneau.direction = 'bas'
+            elif event.key == pygame.K_LEFT and nouvel_anneau.direction != 'droite':
+                nouvel_anneau.direction = 'gauche'
+            elif event.key == pygame.K_RIGHT and nouvel_anneau.direction != 'gauche':
+                nouvel_anneau.direction = 'droite'
+            elif event.key == pygame.K_SPACE:
+                nouvel_anneau.grandir()
     
     screen.fill(ma_fenetre_de_jeu.couleur_de_fond)
-    mon_serpent.deplacer()
-    mon_serpent.afficher()
+    nouvel_anneau.creer()
+    nouvel_anneau.dessiner()
+    nouvel_anneau.grandir()
+
     pygame.display.flip()
     ma_fenetre_de_jeu.tick()
 
