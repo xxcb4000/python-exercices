c3961d985b8054087b40a19a234828fce62783eb
xxcb4000 <vincent.bonhomme@gmail.com>
2025-03-26 09:48:07 +0100

    maj des exos avec lecture/écriture fichier

--- exercice-python-mars.py
+++ exercice-python-mars.py
@@ -168,7 +168,7 @@
 # Everywhere that Mary went The lamb was sure to go","little")
 
 
-print(len('example'))
+#print(len('example'))
 
 # text = "example"
 # length = len(text)  # No AttributeError, correct method usage
@@ -226,47 +226,124 @@ print(len('example'))
 ## Analyse fréquence mots - POO - texte wiki ##
 ###############################################
 
-texte_a_analyser = "La périphrase la plus souvent utilisée pour désigner " \
-"la ville de Liège est « Cité Ardente ». Cette appellation vient du titre " \
-"d'un roman chevaleresque écrit par Henry Carton de Wiart édité en 1904. " \
-"Ce roman raconte le sac de la ville de Liège par les troupes de Charles le " \
-"Téméraire en 1468, malgré la résistance liégeoise, aidée par un important contingent, " \
-"les Six cents Franchimontois, venu d'une seigneurie voisine. L'appellation " \
-"de « Cité Ardente[note 2] » n'est pas antérieure à la parution de ce roman[4]. " \
-"Elle a surtout été popularisée par le prince Albert qui, faisant référence au titre " \
-"dudit roman dans son discours inaugural de l'Exposition universelle de Liège de 1905, " \
-"lance vraiment l'expression auprès des journalistes liégeois. Cette expression est " \
-"restée ancrée dans le langage populaire et la littérature[5]. Outre « Cité Ardente », " \
-"Liège est souvent appelée « La Cité des Princes-évêques » en raison de l'ancienne " \
-"principauté épiscopale de Liège et de l'esprit des Liégeois que l'on qualifie d'esprit " \
-"principautaire. Liège est, tout comme Rouen, Caen, Poitiers, Dijon, Montréal, " \
-"Vienne ou Prague, surnommée la Ville aux Cent Clochers en raison du nombre important " \
-"d'édifices religieux : une cathédrale, six collégiales et une cinquantaine d'églises. " \
-"En raison de ses grands liens d'amitié avec la France — les Liégeois s'étant inspiré de " \
-"la Révolution parisienne de 1789 et ayant par la suite été la première ville étrangère " \
-"à recevoir la Légion d'honneur —, elle est parfois appelée « Le Petit Paris » mais aussi " \
-"« La petite France des bords de Meuse » par Jules Michelet, ou encore « Un petit coin de " \
-"France perdu en Belgique » par Alexandre Dumas. Enfin, Liège a été aussi surnommée " \
-"l'« Athènes du Nord » en raison des écoles qui font sa renommée dans toute l'Europe au Moyen Âge."
+# texte_a_analyser = "La périphrase la plus souvent utilisée pour désigner " \
+# "la ville de Liège est « Cité Ardente ». Cette appellation vient du titre " \
+# "d'un roman chevaleresque écrit par Henry Carton de Wiart édité en 1904. " \
+# "Ce roman raconte le sac de la ville de Liège par les troupes de Charles le " \
+# "Téméraire en 1468, malgré la résistance liégeoise, aidée par un important contingent, " \
+# "les Six cents Franchimontois, venu d'une seigneurie voisine. L'appellation " \
+# "de « Cité Ardente[note 2] » n'est pas antérieure à la parution de ce roman[4]. " \
+# "Elle a surtout été popularisée par le prince Albert qui, faisant référence au titre " \
+# "dudit roman dans son discours inaugural de l'Exposition universelle de Liège de 1905, " \
+# "lance vraiment l'expression auprès des journalistes liégeois. Cette expression est " \
+# "restée ancrée dans le langage populaire et la littérature[5]. Outre « Cité Ardente », " \
+# "Liège est souvent appelée « La Cité des Princes-évêques » en raison de l'ancienne " \
+# "principauté épiscopale de Liège et de l'esprit des Liégeois que l'on qualifie d'esprit " \
+# "principautaire. Liège est, tout comme Rouen, Caen, Poitiers, Dijon, Montréal, " \
+# "Vienne ou Prague, surnommée la Ville aux Cent Clochers en raison du nombre important " \
+# "d'édifices religieux : une cathédrale, six collégiales et une cinquantaine d'églises. " \
+# "En raison de ses grands liens d'amitié avec la France — les Liégeois s'étant inspiré de " \
+# "la Révolution parisienne de 1789 et ayant par la suite été la première ville étrangère " \
+# "à recevoir la Légion d'honneur —, elle est parfois appelée « Le Petit Paris » mais aussi " \
+# "« La petite France des bords de Meuse » par Jules Michelet, ou encore « Un petit coin de " \
+# "France perdu en Belgique » par Alexandre Dumas. Enfin, Liège a été aussi surnommée " \
+# "l'« Athènes du Nord » en raison des écoles qui font sa renommée dans toute l'Europe au Moyen Âge."
 
 
 
-class analyseFrequenceMots:
-    def __init__(self, texte):
-        self.listeMots = texte.replace(',', ' ').replace('.', ' ').replace('[', ' ').replace(']', ' ').replace("'",' ').lower().split(' ')
+# class analyseFrequenceMots:
+#     def __init__(self, texte):
+#         self.listeMots = texte.replace(',', ' ').replace('.', ' ').replace('[', ' ').replace(']', ' ').replace("'",' ').lower().split(' ')
     
-    def frequence_mots(self):
-        frequence_mots = {}
-        for mot in set(self.listeMots):
-            frequence_mots[mot] = self.listeMots.count(mot)
-        return frequence_mots
+#     def frequence_mots(self):
+#         frequence_mots = {}
+#         for mot in set(self.listeMots):
+#             frequence_mots[mot] = self.listeMots.count(mot)
+#         return frequence_mots
     
-    def frequence_un_mot(self, mot):
-        frequence_mot = self.listeMots.count(mot)
-        return frequence_mot
+#     def frequence_un_mot(self, mot):
+#         frequence_mot = self.listeMots.count(mot)
+#         return frequence_mot
 
-monAnalyseur = analyseFrequenceMots(texte_a_analyser)
+# monAnalyseur = analyseFrequenceMots(texte_a_analyser)
 
-print(monAnalyseur.frequence_mots())
-print(monAnalyseur.frequence_un_mot('liège'))
+# print(monAnalyseur.frequence_mots())
+# print(monAnalyseur.frequence_un_mot('liège'))
 
+
+#################################################################
+## Test lecture/écriture fichier - mise à jour fichier membres ##
+#################################################################
+
+
+# my_file = 'my_file.txt'
+# with open(my_file, 'w') as file:
+#     file.write('Coucou mon nouveau fichier\n Et voici une nouvelle ligne \n Bravo Vincent lol mother fucker')
+
+# with open(my_file, 'r') as file:
+#     print(file.read())
+
+
+## on génère les fichiers de membres
+
+# from random import randint
+
+# fichier_des_membres = 'fichier_des_membres.txt'
+# fichier_des_ex_membres = 'fichier_des_ex_membre.txt'
+
+# with open(fichier_des_membres, 'w') as fichier_des_membres:
+#     fichier_des_membres.write('Numero_de_membre     date_inscription      en_ordre\n')
+#     for i in range(20):
+#         numero = str(randint(1,1000))
+#         date_annee = str(randint(1990, 2020))
+#         date_mois = str(randint(1,12))
+#         date_jour = str(randint(1,28))
+#         date = date_jour + '-' + date_mois + '-' + date_annee
+#         en_ordre = 'oui' if bool(randint(0,1)) else 'non'
+#         ligne_traitee = f'{numero:^20}{date:<22}{en_ordre:<10} \n'
+#         fichier_des_membres.write(ligne_traitee)
+
+# with open(fichier_des_ex_membres, 'w') as fichier_des_membres:
+#     fichier_des_membres.write('Numero_de_membre     date_inscription      en_ordre\n')
+#     for i in range(15):
+#         numero = str(randint(1,1000))
+#         date_annee = str(randint(1990, 2020))
+#         date_mois = str(randint(1,12))
+#         date_jour = str(randint(1,28))
+#         date = date_jour + '-' + date_mois + '-' + date_annee
+#         en_ordre = 'non'
+#         ligne_traitee = f'{numero:^20}{date:<22}{en_ordre:<10} \n'
+#         fichier_des_membres.write(ligne_traitee)
+
+## on les met à jour
+
+# fichier_des_membres = 'fichier_des_membres.txt'
+# fichier_des_ex_membres = 'fichier_des_ex_membre.txt'
+# fichier_des_membres_maj = 'fichier_des_membres_maj.txt'
+
+# with open(fichier_des_membres, 'r') as fichier_des_membres:
+#         liste_vrac_fdm = fichier_des_membres.read().split()
+#         liste_tuple_fdm = []
+#         liste_tuple_exfdm_from_fdm = []
+#         for i in range(0,63, 3):
+#             liste_tuple_fdm.append((liste_vrac_fdm[i], liste_vrac_fdm[i+1], liste_vrac_fdm[i+2]))
+#         i = 0
+#         while i <= (len(liste_tuple_fdm)-1):
+#             if liste_tuple_fdm[i][2] == 'non':
+#                 liste_tuple_exfdm_from_fdm.append(liste_tuple_fdm[i])
+#                 del (liste_tuple_fdm[i])
+#                 i -=1 
+#             i+=1
+  
+#         print(liste_tuple_fdm)
+#         print(len(liste_tuple_fdm))
+#         print(liste_tuple_exfdm_from_fdm)
+#         print(len(liste_tuple_exfdm_from_fdm))
+
+# with open(fichier_des_membres_maj, 'w') as fichier_des_membres:
+#      for membre in liste_tuple_fdm:
+#         fichier_des_membres.write(f'{(membre[0]):^20}{str(membre[1]):<22}{str(membre[2]):<10}\n')
+
+# with open(fichier_des_ex_membres, 'a') as fichier_des_ex_membres:
+#      for i in range(0,i):
+#         fichier_des_ex_membres.write(f'{(liste_tuple_exfdm_from_fdm[i][0]):^20}{str(liste_tuple_exfdm_from_fdm[i][1]):<22}{str(liste_tuple_exfdm_from_fdm[i][2]):<10}\n')
