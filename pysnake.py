771cd97eeffad61c9f35a3403a46e5c78a0fc96d
xxcb4000 <vincent.bonhomme@gmail.com>
2025-03-27 14:14:20 +0100

    v1.0.0 menu (à la con), le serpent se déplace nickel, il mange les pommes nickel, il grandit nickel, il se collisionne nickel.... pour la suite : les murs ? un vrai menu ? un anneau gros quand il mange ? les scores ? de la musqie et des sons ?.... etc etc.

--- pysnake.py
+++ pysnake.py
@@ -3,6 +3,7 @@
 #code_source_snake_pythonique = """
 
 import pygame
+from random import randrange
 pygame.init()
 
 #ma classe fenêtre
@@ -18,12 +19,34 @@ class fenetre:
     def tick(self):
         self.horloge.tick(self.fps)
 
-#mon objet fenêtre
-ma_fenetre_de_jeu = fenetre(600,600,(255,255,255), 25, 10)
+
+#boucle du menu
+ma_fenetre_de_jeu = fenetre(600,600,(255,255,255), 15, 10)
 screen = pygame.display.set_mode((ma_fenetre_de_jeu.largeur,ma_fenetre_de_jeu.hauteur))
 
-#ma fonction pour calculer les coordonnées du nouvel anneau du serpent à afficher
-def calcul_nouvelles_coordonnées(position_x, position_y, direction):
+
+#ma classe serpent
+class serpent:
+    def __init__(self, rayon = 5, couleur = (20,148,20), taille= 4, direction = 'droite', index = 0):
+        self.rayon = rayon
+        self.couleur = couleur
+        self.taille = taille
+        self.direction = direction
+        self.positions = [(270,300),(280,300),(290,300),(300,300)]
+        self.index = index
+    
+    def avancer(self):
+        prochaine_position = self.calcul_nouvelles_coordonnées(self.positions[self.index-1][0], self.positions[self.index-1][1], self.direction)
+        if abs(prochaine_position[0] - mes_pommes.position_x) <5 and  abs(prochaine_position[1] - mes_pommes.position_y) <5:
+            mon_serpent.grandir()
+            mes_pommes.couleur = (255,255,255)
+        elif prochaine_position in self.positions:
+            global pause
+            pause = True
+        self.positions[self.index] = self.calcul_nouvelles_coordonnées(self.positions[self.index-1][0], self.positions[self.index-1][1], self.direction)
+        self.index = (self.index + 1) % self.taille
+
+    def calcul_nouvelles_coordonnées(self, position_x, position_y, direction):
         if direction == 'haut':
             if position_y > 0 :
                 position_y -= ma_fenetre_de_jeu.pas
@@ -49,35 +72,64 @@ def calcul_nouvelles_coordonnées(position_x, position_y, direction):
                 position_x = ma_fenetre_de_jeu.largeur
         return(position_x, position_y)
 
-#ma classe serpent
-class serpent:
-    def __init__(self, rayon = 5, couleur = (0,255,0), taille= 4, direction = 'droite', position_init = (300,300), index = 0):
-        self.rayon = rayon
-        self.couleur = couleur
-        self.taille = taille
-        self.direction = direction
-        self.positions = [(270,300),(280,300),(290,300),(300,300)]
-        self.index = index
-    
-    def avancer(self):
-        self.positions[self.index] = calcul_nouvelles_coordonnées(self.positions[self.index-1][0], self.positions[self.index-1][1], self.direction)
-        self.index = (self.index + 1) % self.taille
-
     def dessiner(self):
         for i in range(0, self.taille):
             pygame.draw.circle(screen, self.couleur, (self.positions[i][0], self.positions[i][1]), self.rayon)
 
     def grandir(self):
-        self.positions.append(self.positions[self.index])
+        self.positions.append(self.positions[self.index-1])
         self.taille += 1
 
-#mon objet serpent
-mon_serpent = serpent()
 
+class pomme:
+    def __init__(self, position_x = -10, position_y= -10, rayon = 8, couleur =(255,0,0), duree = 5):
+        self.position_x = position_x
+        self.position_y = position_y
+        self.rayon = rayon
+        self.couleur = couleur
+        self.duree = duree
+        self.apparition = 0
+
+    def ajouter(self):
+        maintenant = pygame.time.get_ticks()
+        if maintenant - self.apparition > self.duree*1000:
+            self.couleur = (255,0,0)
+            self.position_x = randrange(ma_fenetre_de_jeu.pas, ma_fenetre_de_jeu.largeur-ma_fenetre_de_jeu.pas, ma_fenetre_de_jeu.pas)
+            self.position_y = randrange(ma_fenetre_de_jeu.pas, ma_fenetre_de_jeu.hauteur-ma_fenetre_de_jeu.pas, ma_fenetre_de_jeu.pas)
+            self.apparition = pygame.time.get_ticks()
+        pygame.draw.circle(screen, self.couleur, (self.position_x, self.position_y), self.rayon)
+
+
+#initialisation du jeu
+mon_serpent = serpent()
+mes_pommes = pomme()
+global pause
+global running
+global menu
 running = True
+pause = False
+menu = True
 
 #boucle principale du jeu
 while running:
+
+
+    while menu:
+        screen.fill(ma_fenetre_de_jeu.couleur_de_fond)
+        pygame.draw.rect(screen, (56, 111, 72), (ma_fenetre_de_jeu.largeur/2 - 400/2, ma_fenetre_de_jeu.hauteur/2 - 300/2, 400, 300))
+        font = pygame.font.SysFont(None, 20)
+        texte = font.render("Appuyez sur ""Espace"" pour commencer", True, (255, 255, 255))  
+        screen.blit(texte, (150, 250)) 
+        pygame.display.flip()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_SPACE:
+                    menu = False
+                    running = True
+                    pause = False
 
     #gestion des événements
     for event in pygame.event.get():
@@ -95,22 +147,24 @@ while running:
             elif event.key == pygame.K_RIGHT and mon_serpent.direction != 'gauche':
                 mon_serpent.direction = 'droite'
             elif event.key == pygame.K_SPACE:
-                mon_serpent.grandir()
+                menu = True
     
-    #fond blanc
-    screen.fill(ma_fenetre_de_jeu.couleur_de_fond)
+
+
     
     #gestion du serpent
+    if not pause:
+        screen.fill(ma_fenetre_de_jeu.couleur_de_fond)
+        mes_pommes.ajouter()
+        mon_serpent.avancer()
+        mon_serpent.dessiner()
+        #affichage
+        pygame.display.flip()
+        #horloge
+        ma_fenetre_de_jeu.tick()
+    
 
-    mon_serpent.avancer()
-    mon_serpent.dessiner()
-    #mon_serpent.grandir()
 
-    #affichage
-    pygame.display.flip()
-
-    #horloge
-    ma_fenetre_de_jeu.tick()
 
 pygame.quit() #"""
 
